9d3ad2beccf82193fc78f8c163ced2f8ccd4b05e
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-05-26 14:59:03 +0200

    guardamos el JSON de thunder client donde encontramos la solución al problema de la lista de episodios

--- readme.md
+++ readme.md
@@ -5,3 +5,6 @@ Descargar repositorio, y dentro de el :
 npm install ;
 npm run dev ;
 ```
+
+Recursos relevantes empleados :
+[QuickType.io - Para obtener las interfaces de un JSON](https://app.quicktype.io/)
--- src/hooks/useFetch.hook.tsx
+++ src/hooks/useFetch.hook.tsx
@@ -8,6 +8,42 @@ type flag = 'main'|'podcast'|'track';
 interface fetchArgument {route:string,method?:method,body?:any,headers?:any,flag?:flag|undefined}
 const defaultArgument:fetchArgument = {route:'',method:'GET',body:undefined,headers:undefined,flag:undefined}
 
+const storageSave = (data:any,flag:flag|undefined,setState:React.Dispatch<React.SetStateAction<getFetch>>) => {
+    switch(flag){
+        case 'main' :
+            const finalData = data.feed.entry ;
+            setState({data:finalData,isLoading:false,error:null}) ;
+            localStorage.setItem('main',JSON.stringify({date:new Date(),storage:finalData})) ;
+        ; break ;
+        default : break ;
+    }
+};
+
+const localOrNet = (flag:flag|undefined,setState:React.Dispatch<React.SetStateAction<getFetch>>,getFetch:() => void) => {
+        
+    const compareDay = (storage:any):void => {
+        const takeDay = (date:Date) => `${date.getDate}${date.getMonth}${date.getFullYear}`;
+        if(takeDay(storage.date) !== takeDay(new Date())){ getFetch() } else {
+            setState(v => ({data:storage.storage,isLoading:false,error:null}));
+        }
+    }
+
+    if(!flag){ getFetch() }
+
+    switch(flag){
+
+        case 'main':
+            const mainCase:mainInStorage|undefined = ( localStorage.getItem('main') ) ? JSON.parse(`${localStorage.getItem('main')}`) : undefined ;
+            if(mainCase == undefined){ getFetch() ; return } ;
+            compareDay(mainCase) ;
+        ; break ;
+
+        default : getFetch() ; break ;
+
+    }
+
+}
+
 const useFetch = ({route,method,body,headers,flag}:fetchArgument = {...defaultArgument}) => {
 
     const { setLoad } = useContext<{setLoad:(value:boolean) => void}>(mainContext)
@@ -18,46 +54,12 @@ const useFetch = ({route,method,body,headers,flag}:fetchArgument = {...defaultAr
 
         setState({...state,isLoading:true});
         await(await fetch(`${route}`,{method,mode:'cors',body,headers})).json()
-        .then(data => {
-            
-            switch(flag){
-                case 'main' :
-                    const finalData = data.feed.entry ;
-                    setState({data:finalData,isLoading:false,error:null}) ;
-                    localStorage.setItem('main',JSON.stringify({date:new Date(),storage:finalData})) ;
-                ; break ;
-                default : ; break ;
-            }
-
-        })
+        .then(data => { storageSave(data,flag,setState) })
         .catch(error => {setState({data:null,isLoading:false,error}) });
 
     }
 
-    const localOrNet = () => {
-        
-        const compareDay = (storage:any):void => {
-            const takeDay = (date:Date) => `${date.getDate}${date.getMonth}${date.getFullYear}`;
-            if(takeDay(storage.date) !== takeDay(new Date())){ getFetch() } else {
-                setState(v => ({data:storage.storage,isLoading:false,error:null}));
-            }
-        }
-
-        if(!flag){ getFetch() }
-        switch(flag){
-
-            case 'main':
-                const mainCase:mainInStorage|undefined = ( localStorage.getItem('main') ) ? JSON.parse(`${localStorage.getItem('main')}`) : undefined ;
-                if(mainCase == undefined){ getFetch() ; return } ;
-                compareDay(mainCase) ;
-            ; break ;
-
-            default : getFetch() ; break ;
-
-        }
-    }
-
-    useEffect(() => { localOrNet() },[route]);
+    useEffect(() => { localOrNet(flag,setState,getFetch) },[route]);
     useEffect(() => { setLoad(state.isLoading) },[state])
     
     return({...state,getFetch});
